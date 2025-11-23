94aeb285d5bbd3eb02e9e12a8353f78d75073a52
LeCherchologue <le.cherchologue001@gmail.com>
2025-11-23 03:05:23 +0100

    cors

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.router import UsersRouter, ProduitsRouter, CommandesRouter
 from api.bdd.connexion import Base, engine
 from fastapi.staticfiles import StaticFiles
-
-
-
 
 app = FastAPI()
 
@@ -15,7 +11,14 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 origins = [
     "https://noagab-mini-shop.vercel.app",
     "capacitor://localhost",
+    "ionic://localhost",
     "http://localhost",
+    "http://localhost:8100",  # Port par défaut d'Ionic
+    "http://localhost:8080",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8100",
+    "http://10.0.2.2",  # Émulateur Android
+    "http://10.0.2.2:8000",
 ]
 
 app.add_middleware(
@@ -37,4 +40,3 @@ async def main():
     return {
         "message": "API demarre",
     }
-
